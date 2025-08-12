225d3cf1df89ed6a8f6d058c6ddb7b1085ba8a81
MOHD TAUHEED ANSARI <mohdtauheedansari879@gmail.com>
2025-08-12 11:26:47 +0530

    css update

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.getElementById("projects").onclick = function project(){
-    document.getElementById("projects").style.backgroundColor = "white";
-    document.getElementById("projects").style.Color = "black";
-    document.getElementById("work").style.backgroundColor = "transparent";
-    document.getElementById("work").style.Color = "white";
-    document.getElementById("certificate").style.backgroundColor = "transparent";
-    document.getElementById("certificatek").style.Color = "white";
-    document.getElementById("achievement").style.backgroundColor = "transparent";
-    document.getElementById("achievement").style.Color = "white";
+    // document.getElementById("projects").style.backgroundColor = "white";
+    // document.getElementById("projects").style.color = "black";
+    // document.getElementById("work").style.backgroundColor = "transparent";
+    // document.getElementById("work").style.color = "white";
+    // document.getElementById("certificate").style.backgroundColor = "transparent";
+    // document.getElementById("certificatek").style.color = "white";
+    // document.getElementById("achievement").style.backgroundColor = "transparent";
+    // document.getElementById("achievement").style.color = "white";
     document.getElementById("project-items").style.visibility = "visible";
     document.getElementById("work-exp").style.visibility = "hidden";
     document.getElementById("certificate-items").style.visibility = "hidden";
@@ -14,14 +14,14 @@ document.getElementById("projects").onclick = function project(){
 }
 
 document.getElementById("work").onclick = function work(){
-    document.getElementById("projects").style.backgroundColor = "transparent";
-    document.getElementById("projects").style.Color = "white";
-    document.getElementById("work").style.backgroundColor = "white";
-    document.getElementById("work").style.Color = "black";
-    document.getElementById("certificate").style.backgroundColor = "transparent";
-    document.getElementById("certificatek").style.Color = "white";
-    document.getElementById("achievement").style.backgroundColor = "transparent";
-    document.getElementById("achievement").style.Color = "white";
+    // document.getElementById("projects").style.backgroundColor = "transparent";
+    // document.getElementById("projects").style.color = "white";
+    // document.getElementById("work").style.backgroundColor = "white";
+    // document.getElementById("work").style.color = "black";
+    // document.getElementById("certificate").style.backgroundColor = "transparent";
+    // document.getElementById("certificatek").style.color = "white";
+    // document.getElementById("achievement").style.backgroundColor = "transparent";
+    // document.getElementById("achievement").style.color = "white";
     document.getElementById("project-items").style.visibility = "hidden";
     document.getElementById("work-exp").style.visibility = "visible";
     document.getElementById("certificate-items").style.visibility = "hidden";
@@ -29,7 +29,14 @@ document.getElementById("work").onclick = function work(){
 }
 
 document.getElementById("certificate").onclick = function certificate(){
-
+    // document.getElementById("projects").style.backgroundColor = "transparent";
+    // document.getElementById("projects").style.color = "white";
+    // document.getElementById("work").style.backgroundColor = "transparent";
+    // document.getElementById("work").style.color = "white";
+    // document.getElementById("certificate").style.backgroundColor = "white";
+    // document.getElementById("certificate").style.color = "black";
+    // document.getElementById("achievement").style.backgroundColor = "transparent";
+    // document.getElementById("achievement").style.color = "white";
     document.getElementById("project-items").style.visibility = "hidden";
     document.getElementById("work-exp").style.visibility = "hidden";
     document.getElementById("certificate-items").style.visibility = "visible";
@@ -37,7 +44,14 @@ document.getElementById("certificate").onclick = function certificate(){
 }
 
 document.getElementById("achievement").onclick = function achievement(){
-
+    // document.getElementById("projects").style.backgroundColor = "transparent";
+    // document.getElementById("projects").style.color = "white";
+    // document.getElementById("work").style.backgroundColor = "transparent";
+    // document.getElementById("work").style.color = "white";
+    // document.getElementById("certificate").style.backgroundColor = "transparent";
+    // document.getElementById("certificatek").style.color = "white";
+    // document.getElementById("achievement").style.backgroundColor = "white";
+    // document.getElementById("achievement").style.color = "black";
     document.getElementById("project-items").style.visibility = "hidden";
     document.getElementById("work-exp").style.visibility = "hidden";
     document.getElementById("certificate-items").style.visibility = "hidden";
